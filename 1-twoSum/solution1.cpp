6e02c0b1e1b20e70fd1d54ee1f0b902d595a96c9
zhangxiaoyang <zhangxiaoyang.hit@gmail.com>
2015-04-08 13:27:57 +0800

    modified direcory

---
+++ 1-twoSum/solution1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &vNumbers, int target) {
+        vector<int> vResults;
+        map<int, unsigned int> mTable;
+
+        for(unsigned int i = 0; i < vNumbers.size(); i++) {
+            if(!mTable.count(vNumbers[i])) {
+                mTable.insert(pair<int, unsigned int>(vNumbers[i], i));
+            }
+            if(mTable.count(target - vNumbers[i]) && mTable[target - vNumbers[i]] < i) {
+                vResults.push_back(mTable[target - vNumbers[i]] + 1);
+                vResults.push_back(i + 1);
+                cout<<vResults[0]<<","<<vResults[1]<<endl;
+                return vResults;
+            }
+        }
+        return vResults;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    //int numbers[] = {7, 2, 11, 15};
+    //int target = 9;
+    int numbers[] = {3,2,4};
+    int target=6;
+
+    vector<int> vNumbers(numbers, numbers + 5);
+    Solution().twoSum(vNumbers, target);
+    return 0;
+}
+
