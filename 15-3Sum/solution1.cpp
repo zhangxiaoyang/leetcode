00128d4a0657e26adc63333b40289b02973195d4
zhangxiaoyang <zhangxiaoyang.hit@gmail.com>
2015-04-25 16:39:33 +0800

    solved problem 15

--- 15-3Sum/solution1.cpp
+++ 15-3Sum/solution1.cpp
@@ -1,40 +1,49 @@
 #include <iostream>
 #include <vector>
-#include <set>
-#define UINT unsigned int
+#include <algorithm>
+#include <functional>
 using namespace std;
 
 class Solution {
 public:
     vector<vector<int> > threeSum(vector<int> &num) {
         vector<vector<int> > result;
-        set<int> num2;
-        for(vector<int>::iterator i = num.begin(); i != num.end(); i++) {
-           num2.insert(*i);
-        }
+        sort(num.begin(), num.end(), less<int>());
 
-        for(set<int>::iterator i = num2.begin(); i != num2.end(); i++) {
-            for(set<int>::iterator j = i + 1; j != num2.end(); j++) {
-                for(set<int>::iterator k = j + 1; k != num2.end(); k++) {
-                    if(*i + *j + *k == 0) {
-                        vector<int> v;
-                        v.push_back(*i);
-                        v.push_back(*j);
-                        v.push_back(*k);
-                        cout<<*i<<" "<<*j<<" "<<*k<<endl;
-                        result.push_back(v);
-                    }
+        for(unsigned int i = 0; i < num.size(); i++) {
+            if(i > 0 && num[i] == num[i-1])
+                continue;
+
+            for(unsigned int j = i+1; j < num.size(); j++) {
+                if(j > i+1 && num[j] == num[j-1])
+                    continue;
+                int number = 0-num[i]-num[j];
+
+                if(!(find(num.begin()+j+1, num.end(), number) == num.end())) {
+                    vector<int> tmp;
+                    tmp.push_back(num[i]);
+                    tmp.push_back(num[j]);
+                    tmp.push_back(number);
+                    sort(tmp.begin(), tmp.end(), less<int>());
+                    result.push_back(tmp);
                 }
             }
         }
-
         return result;
     }
 };
 
 int main(int argc, char* argv[]) {
-    int array[] = {-1, 0, 1, 2, -1, -4};
-    vector<int> num(array, array+6);
-    Solution().threeSum(num);
+    int array[] = {-1, 0, 1, 2, -1, -4, 1, -1};
+    vector<int> num(array, array+8);
+    vector<vector<int> > result = Solution().threeSum(num);
+
+    cout<<result.size()<<endl;
+    for(unsigned int i = 0; i < result.size(); i++) {
+        for(unsigned int j = 0; j < result[i].size(); j++) {
+            cout<<result[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
