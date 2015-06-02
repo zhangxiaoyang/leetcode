abded2e17fbd7a825678a86959321a902bd05ca9
zhangxiaoyang <zhangxiaoyang.hit@gmail.com>
2015-06-02 15:31:11 +0800

    added template

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+class Solution {
+public:
+};
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
