b04b1fb2b3746d6e28fc9a6b9619974d05c325aa
zhangxiaoyang <zhangxiaoyang.hit@gmail.com>
2015-06-04 15:15:43 +0800

    bug 85

---
+++ 85-maximalRectangle/solution1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int length = (int)
+        for
+    }
+};
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
